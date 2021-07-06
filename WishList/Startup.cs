4ddf2d242e4e629eac41eb93be802e9bfaaaa8e8
Paris Pantigoso <parismiguel@gmail.com>
2021-07-06 17:03:55 -0500

    Create Item Model With EntityFramework Support

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -11,6 +13,10 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("WishList");
+            });
             services.AddMvc();
         }
 
